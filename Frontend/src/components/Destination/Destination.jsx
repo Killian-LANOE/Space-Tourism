a74a9b5f960cf3c89ce102deaed4c5404dfb8c0f
Killian Lanoe <killian.lanoe98@gmail.com>
2023-07-25 17:31:34 +0200

    Started Destination page
    Added planets infos,
    Created planets choice function

--- Frontend/src/components/Destination/Destination.jsx
+++ Frontend/src/components/Destination/Destination.jsx
@@ -1,5 +1,31 @@
+import data from "../../../../Backend/data.json";
+import { useState } from "react";
+
 function Destination() {
-  return <h1 id="Destination">Destination</h1>;
+  const [planet, setPlanet] = useState(0);
+  const destination = data.destinations;
+
+  return (
+    <div id="Destination">
+      <div id="Destination--content">
+        <img src={destination[planet].images.png}></img>
+        <div id="Destination--content--choice">
+          <button onClick={() => setPlanet(0)}>Moon</button>
+          <button onClick={() => setPlanet(1)}>Mars</button>
+          <button onClick={() => setPlanet(2)}>Europa</button>
+          <button onClick={() => setPlanet(3)}>Titan</button>
+        </div>
+        <h1>{destination[planet].name}</h1>
+        <p>{destination[planet].description}</p>
+        <p className="Destination--content--info">
+          AVG. DISTANCE <span>{destination[planet].distance}</span>
+        </p>
+        <p className="Destination--content--info">
+          EST. TRAVEL TIME <span>{destination[planet].travel}</span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Destination;
