7981339a3ea9869c8d147d38dd2e8299c27a40b7
Killian Lanoe <killian.lanoe98@gmail.com>
2023-07-27 18:39:54 +0200

    Did mobile version of Technology page

--- Frontend/src/components/Technology/Technology.jsx
+++ Frontend/src/components/Technology/Technology.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import data from "../../../../Backend/data.json";
+
 function Technology() {
-  return <h1 id="Technology">Technology</h1>;
+  const [transport, setTransport] = useState(0);
+  const technology = data.technology;
+
+  return (
+    <div id="Technology">
+      <p id="Technology--title">
+        <span>03</span> SPACE LAUNCH 101
+      </p>
+      <div id="Technology--content">
+        <img
+          src={technology[transport].images.landscape}
+          alt={`image of the ${technology[transport].name}`}
+        ></img>
+        <div id="Technology--content--menu">
+          <button onClick={() => setTransport(0)}>1</button>
+          <button onClick={() => setTransport(1)}>2</button>
+          <button onClick={() => setTransport(2)}>3</button>
+        </div>
+        <p id="Technology--content--terminology">THE TERMINOLOGY...</p>
+        <h1>{technology[transport].name}</h1>
+        <p id="Technology--content--description">
+          {technology[transport].description}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Technology;
