b7fdbe3e3e79460ef060b8a01fb389093a65d66c
Killian Lanoe <killian.lanoe98@gmail.com>
2023-08-12 18:09:50 +0200

    added css responsives / added function isTablet
    Added a responsives media queries for tablet & desktop
    
    Changed the isMobile function to IsTablet as nav change in tablet version

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -12,42 +12,55 @@ function Navbar() {
     menu.classList.toggle("hidden");
   }
 
-  const isMobile = window.innerWidth < 426;
+  const isTablet = window.innerWidth > 425 && window.innerWidth < 1024;
 
   return (
-    <nav className="absolute h-20 w-full pb-6 text-white font-other justify-between z-10">
+    <nav className="absolute h-20 w-full pb-6 text-white font-other justify-between z-10 lg:h-24 lg:mt-[40px]">
       <div className="grid h-20 w-11/12 m-auto grid-flow-col items-center justify-between">
         <img
           id="logo"
           src="Frontend/src/assets/shared/logo.svg"
           alt="logo of space tourism"
-          className=""
+          className="mt-6 ml-2"
         ></img>
-        <button className="h-6 w-6 " onClick={handleMenu}>
+        <button className="h-6 w-6 md:hidden " onClick={handleMenu}>
           <img src="Frontend/src/assets/shared/icon-hamburger.svg"></img>
         </button>
       </div>
 
       <div
         id="menu"
-        className="absolute translate-x-full hidden right-0 top-0 flex-col overflow-hidden w-2/3"
+        className="absolute translate-x-full hidden right-0 top-0 flex-col overflow-hidden w-2/3 md:translate-x-0 md:flex md:h-24 md:flex-row md:w-2/3 lg:w-4/6"
       >
-        <aside className="backdrop-blur-3xl h-screen w-full">
-          <button className="absolute right-6 top-8 " onClick={handleMenu}>
+        <aside className="md:absolute backdrop-blur-2xl h-screen w-full md:bg-white md:bg-opacity-5 lg:right-0 lg:w-5/6 lg:h-full">
+          <button
+            className="absolute right-6 top-8 md:hidden "
+            onClick={handleMenu}
+          >
             <img src="Frontend/src/assets/shared/icon-close.svg"></img>
           </button>
 
-          <div className="flex flex-col h-3/5 justify-center gap-8 ml-8 tracking-[2.7px]">
+          <div className="flex flex-col h-3/5 justify-center gap-8 ml-8 tracking-[2.7px] md:flex-row md:items-center md:h-24 md:w-2/3 md:mx-auto lg:ml-38 lg:gap-12">
             {menuPages.map((page, index) => (
               <a
                 key={index}
-                className={`${
-                  window.location.pathname === page.link ? "active" : ""
-                }`}
+                className="relative group md:relative"
                 href={page.link}
               >
-                {isMobile ? <strong className="mr-3">0{index}</strong> : ""}{" "}
+                <span
+                  className={`md:absolute md:h-[3px] md:w-full md:bg-white md:-bottom-9 md:left-0 ${
+                    window.location.pathname === page.link
+                      ? "md:opacity-1"
+                      : "md:opacity-0"
+                  }`}
+                ></span>
+                <strong className={isTablet ? "hidden" : "mr-2"}>
+                  0{index}
+                </strong>
                 {page.text}
+                <span
+                  className={`md:absolute md:inset-x-0 md:-bottom-9 md:h-[3px] md:bg-white/25 md:opacity-0 md:group-hover:opacity-100`}
+                ></span>
               </a>
             ))}
           </div>
